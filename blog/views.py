d9a615dc176d0859df0ef3830edaa7a25c42fa6c
csdavids519 <csdavds@proton.me>
2024-10-17 14:46:26 +0200

    Add custom static css files

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,6 @@ from .models import Post
 
 # Create your views here.
 class PostList(generic.ListView):
-   queryset = Post.objects.filter(author=2)
-       # template_name = "post_list.html"
-
+    queryset = Post.objects.filter(author=2)
+    template_name = "blog/index.html"
+    paginate_by = 6
