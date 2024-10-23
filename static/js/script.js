e6ec6e3ef9c8a820f952f78e6839bd97c8164884
csdavids519 <csdavds@proton.me>
2024-10-23 16:37:30 +0200

    Added comment CRUD

--- static/js/script.js
+++
@@ -1,2 +0,0 @@
-console.log("42");
-console.log("Life, The Universe and Everything!");
--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -1,2 +1,1 @@
-console.log("42");
-console.log("Life, The Universe and Everything!");
+console.log("It works!");
