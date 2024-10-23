1650b5040038d11f49bc6bd2415da1698794e6f5
csdavids519 <csdavds@proton.me>
2024-10-23 15:51:25 +0200

    Added forms.py to about section

--- blog/views.py
+++ blog/views.py
@@ -30,18 +30,20 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
+        print("Received a POST request")
         comment_form = CommentForm(data=request.POST)
-    if comment_form.is_valid():
-        comment = comment_form.save(commit=False)
-        comment.author = request.user
-        comment.post = post
-        comment.save()
-        messages.add_message(
-            request, messages.SUCCESS,
-            'Comment submitted and awaiting approval'
-    )
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
 
     comment_form = CommentForm()
+    print("About to render template")
 
     return render(
         request,
